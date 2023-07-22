dfe167e35073346fe160fd00821fe336991ac071
Alex8804 <128159385+Alex8804@users.noreply.github.com>
2023-07-22 18:43:30 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
 const listWithId = document.querySelector('#categories');
+
 const listItemNumber = listWithId.children.length;
 console.log(`Number of categories: ${listItemNumber}`);
 
-const listItem = listWithId.children;
-const listItemArray = Array.from(listItem);
+const listItems = [...listWithId.children];
 
-listItemArray.forEach(elements => {
+listItems.forEach(elements => {
   console.log(`Category: ${elements.firstElementChild.textContent}`);
-  console.dir(`Elements: ${elements.lastElementChild.children.length}`);
+  console.log(`Elements: ${elements.lastElementChild.children.length}`);
 });
